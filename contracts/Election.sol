af437f45da3b19af35e30ea283c9b75a45be6b90
christian bell <christiantcbell@gmail.com>
2020-08-06 13:30:43 +0100

    updated election contract and added tests

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,26 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Election {
-    // Read/write candidate
-    string public candidate;
+    // Model a candidate
 
+    struct Candidate {
+      uint id;
+      string name;
+      uint voteCount;
+    }
+    // Store a candidate
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store a candidates count
+    uint public candidatesCount;
     // Constructor
     constructor() public {
-        candidate = "Candidate 1";
+      addCandidate("Candidate 1");
+      addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+      candidatesCount ++;
+      candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
